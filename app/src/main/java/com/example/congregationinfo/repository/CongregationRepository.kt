0a17dd5a9b597bf1bfc639704d239ff226ed39ec
Attila <pappattil@freemail.hu>
2021-12-22 11:02:17 +0100

    updated gradle dependencies for retrofit2

--- app/src/main/java/com/example/congregationinfo/repository/CongregationRepository.kt
+++ app/src/main/java/com/example/congregationinfo/repository/CongregationRepository.kt
@@ -1,9 +1,6 @@
 package com.example.congregationinfo.repository
 
-import androidx.lifecycle.MutableLiveData
-import com.example.congregationinfo.data.CongregationData
 import com.example.congregationinfo.datasource.CongregationNetworkDataSource
-import com.example.congregationinfo.ui.CongregationViewState
 import com.example.congregationinfo.util.NetworkResult
 
 
