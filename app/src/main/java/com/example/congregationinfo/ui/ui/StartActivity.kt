f3c4523ed7dd008022dd71b353ca22a84b9dec40
Vorák Gergő <vorakgeri@gmail.com>
2021-12-29 13:49:10 +0100

    hardcode hint and options comment off

--- app/src/main/java/com/example/congregationinfo/ui/ui/StartActivity.kt
+++ app/src/main/java/com/example/congregationinfo/ui/ui/StartActivity.kt
@@ -18,13 +18,13 @@ class StartActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityStartBinding.inflate(layoutInflater)
         setContentView(binding.root)
-/*        setSupportActionBar(findViewById(R.id.toolbar))
+        /*        setSupportActionBar(findViewById(R.id.toolbar))
         findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = title
-       findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
-*/
+        */
         if (Global.resultDate != "") binding.tvDataStatus.text="Adatok frissítve:\n"+Global.resultDate
 
         binding.btnCongregation.setOnClickListener {
@@ -35,7 +35,6 @@ class StartActivity : AppCompatActivity() {
         binding.btnName.setOnClickListener {
             val intent = Intent(this@StartActivity, NameActivity::class.java)
             startActivity(intent)
-
         }
 
         binding.btnMinisry.setOnClickListener {
@@ -43,22 +42,4 @@ class StartActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
-/*
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_scrolling, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-*/
 }
