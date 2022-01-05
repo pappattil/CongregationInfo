d4b4bf40d6898619eb2a56a64520d72be72acd54
Attila <pappattil@freemail.hu>
2022-01-05 19:54:21 +0100

    login with firebase

--- app/src/main/java/com/example/congregationinfo/ui/ui/LoginActivity.kt
+++ app/src/main/java/com/example/congregationinfo/ui/ui/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.congregationinfo.ui.ui
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,25 +10,24 @@ import com.example.congregationinfo.data.AppDatabase
 import com.example.congregationinfo.data.CongregationDataRoom
 import com.example.congregationinfo.data.Global
 import com.example.congregationinfo.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 import kotlin.concurrent.thread
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityLoginBinding
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         thread {
             var congRoomAll: CongregationDataRoom
             try{
                 congRoomAll =
                 AppDatabase.getInstance(this@LoginActivity).congDao().getAllInfo().last()
             }catch(e: Exception){
-                congRoomAll= CongregationDataRoom(null,"",0,"55555",Global.resultDate,Global.resultValues)
+                congRoomAll= CongregationDataRoom(null,"",1,"",Global.resultDate,Global.resultValues)
                 AppDatabase.getInstance(this@LoginActivity).congDao().insertInfo(congRoomAll)
             }
-
             Global.name = congRoomAll.name
             Global.firstStartCounter = congRoomAll.firstStartCounter
             Global.HARDD_CODE = congRoomAll.hardCode
@@ -35,7 +35,7 @@ class LoginActivity : AppCompatActivity() {
             Global.resultValues=congRoomAll.resultValues
 
             runOnUiThread {
-                if(Global.firstStartCounter==1) {
+                if(Global.firstStartCounter==0) {
                     nextActivity(Intent(this@LoginActivity,StartActivity::class.java))
                 }
                 else{
@@ -43,32 +43,7 @@ class LoginActivity : AppCompatActivity() {
                     binding.btnLogin.visibility = View.VISIBLE
                     binding.textInputLayout.visibility = View.VISIBLE
                     binding.loginTextView.visibility = View.VISIBLE
-                }
-            }
-        }
-
-        binding.loginTextView.text="Add meg a kódot"
-
-        binding.btnLogin.setOnClickListener {
-            when {
-                Global.HARDD_CODE == binding.etLogin.text.toString() -> {
-                    thread{
-                        val congRoom = CongregationDataRoom(null,"",1,"55555",Global.resultDate,Global.resultValues)
-                        AppDatabase.getInstance(this@LoginActivity).congDao().deleteAll()
-                        AppDatabase.getInstance(this@LoginActivity).congDao().insertInfo(congRoom)
-                    }
-                    Global.firstStartCounter = 1
-                    nextActivity(Intent(this@LoginActivity,NameActivity::class.java))
-                }
-                Global.firstStartCounter > 10 -> {
-                    binding.loginTextView.text="Túl sokszor adtál meg helytelen kódot. "
-                    binding.btnLogin.visibility = View.GONE
-                    binding.textInputLayout.visibility = View.GONE
-                    binding.loginTextView.visibility = View.VISIBLE
-                }
-                else -> {
-                    Global.firstStartCounter++
-                    Toast.makeText(this, "Nem megfelelő kód!\n ",Toast.LENGTH_LONG).show()
+                    binding.textInputLayout2.visibility = View.VISIBLE
                 }
             }
         }
@@ -78,5 +53,44 @@ class LoginActivity : AppCompatActivity() {
         val activityToClose = this@LoginActivity
         startActivity(intent)
         activityToClose.finish()
+    }
+
+    private fun isFormValid(): Boolean{
+      if(binding.etLoginName.text.isEmpty()){
+            binding.etLoginName.error = "Ez a mező nem maradhat üres!"
+            return false
+        } else return true
+    }
+
+    fun loginClick(view: android.view.View) {
+        if(!isFormValid()){
+            return
+        }
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(
+            binding.etLoginName.text.toString(), binding.etpassword.text.toString()
+        ).addOnSuccessListener {
+            thread {
+                   val congRoom = CongregationDataRoom(
+                       null,
+                       Global.name,
+                       0,
+                       it.user!!.email.toString(),
+                       Global.resultDate,
+                       Global.resultValues )
+                   AppDatabase.getInstance(this@LoginActivity).congDao().deleteAll()
+                   AppDatabase.getInstance(this@LoginActivity).congDao().insertInfo(congRoom)
+               }
+                Global.firstStartCounter = 0
+                nextActivity(Intent(this@LoginActivity,NameActivity::class.java))
+        }.addOnFailureListener {
+            Toast.makeText(this@LoginActivity,"Login Error",Toast.LENGTH_LONG).show()
+            Global.firstStartCounter++
+            if (Global.firstStartCounter == 10){
+                binding.textInputLayout.visibility=View.GONE
+                binding.textInputLayout2.visibility=View.GONE
+                binding.btnLogin.visibility=View.GONE
+                binding.loginTextView.text = "Túl sokszor adtál meg halytelen kódot!"
+            }
+        }
     }
 }
